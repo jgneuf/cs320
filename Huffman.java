02a9ef779eb933ae36bc0016305a40db3f3bb6d3
Jonathan <ksk.logan@hotmail.com>
2011-10-11 18:05:39 -0700

    Code refactored and polished.

--- Huffman.java
+++ Huffman.java
@@ -7,35 +7,29 @@ import java.util.PriorityQueue;
 
 /** This class depends on BinaryOut.java, a class I did not write myself. As far as I am
  * aware, there is no other (simple) way to write bits to a file with Java. This class
- * was created by Princeton University. For additional information on other library files
- * and BinaryOut.java, please see:
- * http://introcs.cs.princeton.edu/java/stdlib/
- */
+ * was created at Princeton University. For additional information on other library files
+ * and BinaryOut.java, please see: http://introcs.cs.princeton.edu/java/stdlib/       */
 public class Huffman {
 	private int[]                         freq;       // Store frequency of each character
 	private String[]                      codes;      // Array holding codes of each character
-	private PriorityQueue<CharFrequency>  heap;		  // Heap for character/frequencies
+	private PriorityQueue<CharFrequency>  heap;		  // Heap for character-frequencies
 	private String                        text;       // Text to compress
-	private String                        outputFile; // Where to write compressed text
+	private String                        file;       // Where to write compressed text
 	private final static int              ALPH = 128; // Alphabet size 
 
-	/** Set up Huffman to work with the given string. By default,
-	 * use a 128-character alphabet, the ASCII character set.
-	 * 
-	 * @param s String to compress.
+	/** Huffman will run on the given string and output the result to the given file.
+	 * @param file Name of output file.
+	 * @param text String to compress.
 	 */
 	public Huffman (String file, String text) {
-		freq = new int[ALPH];
-		for (int i = 0; i < ALPH; i++)
-			freq[i] = 0;
-
-		codes      = new String[ALPH];
-		this.text  = text;
-		outputFile = file + ".hff";
-		heap  = new PriorityQueue<CharFrequency>(ALPH, new CharFrequencyComparator());
+		this.text = text;
+		this.file = file + ".hff";
+		codes     = new String[ALPH];
+		freq      = new int[ALPH];
+		heap      = new PriorityQueue<CharFrequency>(ALPH, new CharFrequencyComparator());
 	}
 
-	/** Allow everything to be reclaimbed by the garbage collector. */
+	/** Allow everything to be reclaimed by the garbage collector. */
 	public void clean () {
 		freq  = null;
 		text  = null;
@@ -43,41 +37,37 @@ public class Huffman {
 		heap  = null;
 	}
 
-	/** Return a bit-sequence for the string given to Huffman. */
+	/** Compress the string and write it to an output file. */
 	public void encode () {
 		compress();
 		write();
-
-		/*
-		String retVal = "";
-		for (int i = 0; i < text.length(); i++)
-			retVal += codes[text.charAt(i)];
-		return retVal;
-		 */
 	}
 
-	/** Compress the given string. */
+	/** Compress the string. */
 	private void compress () {
-		// Increment frequency of each character in text.
+		// Increment frequency of each character in the text.
 		for (int i = 0; i < text.length(); i++)
 			freq[(int) text.charAt(i)]++;
 
-		// Build a heap with each character.
+		// Build heap with one node per character.
 		for (int i = 0; i < ALPH; i++) {
 			if (freq[i] > 0)
 				heap.add(new CharFrequency((char) i, freq[i]));
 		}
 
-		// Rebuild the heap.
+		// Rebuild the heap into Huffman tree. The result is a heap with a
+		// single element, but it is the root node of the Huffman tree. This
+		// can be used later to access the tree.
 		while (heap.size() > 1) {
 			CharFrequency x = heap.poll();
 			CharFrequency y = heap.poll();
 
-			CharFrequency z = new CharFrequency((char) 0, x.getFreq() + y.getFreq());
+			CharFrequency z = new CharFrequency((char) 0, 
+					x.frequency + y.frequency);
 
 			heap.add(z);
-			z.setLeft(y);
-			z.setRight(x);
+			z.left  = y;
+			z.right = x;
 		}
 
 		// Create Huffman bit-sequence for string.
@@ -86,9 +76,17 @@ public class Huffman {
 
 	/** Write the compressed text to a file. */
 	private void write() {
-		BinaryOut bout = new BinaryOut(outputFile);
+		// Prepare a binary stream to a file.
+		BinaryOut bout = new BinaryOut(file);
+		
+		// Get the bit-sequence for each character in the text. It's a string
+		// representing bits. For each element of the string, write a '0' or
+		// '1' to the output file.
 		for (int i = 0; i < text.length(); i++) {
+			// Get the bit-sequence for the character.
 			String c = codes[text.charAt(i)];
+			
+			// Now write the bit to the file.
 			for (int j = 0; j < c.length(); j++) {
 				if (c.charAt(j) == '0')
 					bout.write(true);
@@ -100,25 +98,21 @@ public class Huffman {
 	}
 
 	/** In-order traversal of Huffman tree to set bit-sequence for each node. */
-	private void inorder (CharFrequency parent, String bit) {
-		if (parent != null) {
-			parent.setBitSequence(bit);
+	private void inorder (CharFrequency node, String bits) {
+		// Stop traversal when the reference is null.
+		if (node != null) {
+			node.bitSequence = bits;
 
-			if (parent.isExternal())
-				codes[(int) parent.getChar()] = bit;
-			inorder(parent.getLeft(), parent.getBitSequence() + "0");
-			inorder(parent.getRight(), parent.getBitSequence() + "1");
+			// External nodes hold characters and a full bit-sequence. Store this
+			// bit sequence in a codes array so we can lookup the bit-sequence for
+			// this character quickly later on.
+			if (node.isExternal())
+				codes[(int) node.character] = bits;
+			
+			// Left children add a '0', right children add a '1' to their bit-sequence.
+			inorder(node.left, node.bitSequence + "0");
+			inorder(node.right, node.bitSequence + "1");
 		}
-	}
-
-	/** Print each character and its frequency in the string. */
-	public String getCharacterFrequencies () {
-		String retVal = "";
-		for (int i = 0; i < ALPH; i++) {
-			if (codes[i] != null)
-				retVal += (char) i + " = " + codes[i] + "\n";
-		}
-		return retVal;
 	}
 
 	/** Nested class representing a key-value pair. Here the key is
@@ -138,86 +132,48 @@ public class Huffman {
 			bitSequence  = null;
 		}
 
-		/** Get character. */
-		public char getChar () { return character; } 
-
-		/** Get frequency. */
-		public int getFreq () { return frequency; }
-
-		/** Set bitsequence. */
-		public void setBitSequence (String bs) { bitSequence = bs; }
-
-		/** Get bitsequence. */
-		public String getBitSequence () { return bitSequence; }
-
-		/** Get right child. */
-		public CharFrequency getRight () { return right; }
-
-		/** Get left child. */
-		public CharFrequency getLeft () { return left; }
-
-		/** Set right child. */
-		public CharFrequency setRight (CharFrequency child) {
-			right = child;
-			return right;
-		}
-
-		/** Set left child. */
-		public CharFrequency setLeft (CharFrequency child) {
-			left = child;
-			return left;
-		}
-
 		/** Return true if node is external. */
-		public boolean isExternal () {
-			return (left == null) && (right == null);
-		}
-
-		/** Return a string representation of a CharFrequency. */
-		public String toString () {
-			String retVal = "(char = " + character + ", freq = "
-					+ frequency + ", bit-sequence: " + bitSequence + ")\n";
-			return retVal;
-		}
+		public boolean isExternal () { return (left == null) && (right == null); }
 	}
 
 	/** Comparator for CharFrequency. Need this for the heap. */
 	private class CharFrequencyComparator implements Comparator<CharFrequency> {
 		public int compare(CharFrequency cf1, CharFrequency cf2) {
-			return cf1.getFreq() - cf2.getFreq();
+			return cf1.frequency - cf2.frequency;
 		}
 	}
 
 	/** Take arguments from command line and run Huffman on those files. */
 	public static void main (String[] args) {
-		if (args.length < 2)
+		// Check that the user provided some files to compress. Don't run if there
+		// aren't any.
+		if (args.length == 0)
 			System.out.println("Usage: Provide files to compress as arguments.");
 		
 		// Run Huffman on each given file.
 		for (String file : args) {
 			String input = "";
+			
+			// Read the file into one big string.
 			try {
 				System.out.println("--- Analyzing " + file + " ---");
+				
 				// Prepare to read file.
 				FileInputStream ifs1 = new FileInputStream(file);
 				DataInputStream dis1 = new DataInputStream(ifs1);
 				BufferedReader  br1  = new BufferedReader(new InputStreamReader(dis1));
 
-				// Read first file into a string.
+				// Read file into a string.
 				String tempString;
 				System.out.println("Reading...");
-				while ((tempString = br1.readLine()) != null) {
+				while ((tempString = br1.readLine()) != null)
 					input += tempString;
-				}
 				dis1.close();
-
-			} 
-			// Catch and print any errors during reading.
-			catch (Exception e) {
+			} catch (Exception e) {
 				System.err.println("Error: " + e.getMessage());
 			}
 
-			// Get the Huffman coding length and compare to expected ASCII bit length.
+			// Run Huffman on the file.
 			Huffman hf = new Huffman(file, input);
 			System.out.println("Coding...");
 			hf.encode();
